3afe9b87a483fe2a2c15aabd233461d939ec3234
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-09-26 13:35:50 +0700

    Add files via upload

---
+++ PBOUlang_10116584_Latihan1_Biodata.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116584_Latihan1_Biodata;
+
+/**
+ *
+ * @author
+ * NIM      : 10116584
+ * NAMA     : Muhammad Syahrun Dalu
+ */
+public class PBOUlang_10116584_Latihan1_Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR");
+        System.out.println("---------------------");
+        System.out.println("NAMA            : Muhammad Syahrun Dalu");
+        System.out.println("ALAMAT          : Jalan Dago Pojok No.28A");
+        System.out.println("TEMPAT LAHIR    : Kabila");
+        System.out.println("TANGGA LAHIR    : 24 November 1996");
+        System.out.println("---------------------");
+        System.out.println("HOBI            : Musik Dan Foto");
+        System.out.println("MAKES           : Nasi Goreng");
+        System.out.println("MIKES           : Kopi");
+    }
+    
+}
